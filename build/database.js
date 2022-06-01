926f257ed0c38b56aed51c323fb43fc454bb69b5
Luis Francisco Gil Pérez <luis.gil@resser.com>
2022-06-01 12:58:28 -0500

    database connection

--- build/database.js
+++ build/database.js
@@ -1,8 +1,10 @@
 "use strict";
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/yomrest")
+mongoose
+    .connect("mongodb+srv://luisgil:Yomrest_2022@yomrest.12olw.mongodb.net/?retryWrites=true&w=majority")
     .then((db) => {
     console.log("Connected to MongoDB:", db.connections[0].name);
-}).catch((err) => {
+})
+    .catch((err) => {
     console.log("Error connecting to MongoDB: ", err);
 });
--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/yomrest")
-    .then((db: any) => {
-    console.log("Connected to MongoDB:",db.connections[0].name);
-}).catch((err: any) => {
+mongoose
+  .connect(
+    "mongodb+srv://luisgil:Yomrest_2022@yomrest.12olw.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then((db: any) => {
+    console.log("Connected to MongoDB:", db.connections[0].name);
+  })
+  .catch((err: any) => {
     console.log("Error connecting to MongoDB: ", err);
-});
+  });
