25c0205321489685995266fb5002415252fd58ac
Luis Francisco Gil Pérez <luis.gil@resser.com>
2022-06-01 14:52:05 -0500

    Sensitive

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    "mongodb+srv://luisgil:Yomrest_2022@yomrest.12olw.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://<username>:<password>@<database>/?retryWrites=true&w=majority"
   )
   .then((db: any) => {
     console.log("Connected to MongoDB:", db.connections[0].name);
